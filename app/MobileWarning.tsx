e0142976937a5ab689af13d814a756bfba4faeac
Armaan Thakur <armaanthakur@Armaans-MacBook-Air.local>
2025-01-30 16:09:08 +0530

    mobile restriction2

--- app/MobileWarning.tsx
+++ app/MobileWarning.tsx
@@ -1,17 +1,15 @@
 'use client'
 import { useRouter } from "next/navigation";
-import React, { useState, useEffect } from "react";
+import React, {  useEffect } from "react";
 // import { useRouter } from "next/router";
 
 const MobileWarning = () => {
-  const [isMobile, setIsMobile] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     const checkMobile = () => {
       const isMobileDevice =
         window.innerWidth <= 768 || /Mobi|Android/i.test(navigator.userAgent);
-      setIsMobile(isMobileDevice);
 
       if (isMobileDevice) {
         // Only redirect after router is ready
--- app/not-optimized/page.tsx
+++ app/not-optimized/page.tsx
@@ -5,13 +5,13 @@ const NotOptimizedPage = () => {
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 text-white px-6 py-8">
       <div className="text-center bg-gray-800 p-6 rounded-xl shadow-xl max-w-md w-full">
         <h1 className="text-2xl font-bold text-gray-200 mb-4">
-          Oops! This website isn't optimized for mobile yet 📱
+          Oops! This website isn&apos;t optimized for mobile yet 📱
         </h1>
         <p className="text-lg text-gray-400 mb-6">
-          We're sorry for the inconvenience 😔. For the best experience, please view this site on a desktop or laptop.
+          We&apos;re sorry for the inconvenience 😔. For the best experience, please view this site on a desktop or laptop.
         </p>
         <p className="text-md text-gray-500">
-          (If you're already on a laptop, please refresh the page! 🔄)
+          (If you&apos;re already on a laptop, please refresh the page! 🔄)
         </p>
       </div>
     </div>
