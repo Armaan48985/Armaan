32e5a3dd1c5666d701bebff2c6d4ef40362371bd
Armaan Thakur <armaanthakur@Armaans-MacBook-Air.local>
2025-02-03 14:28:20 +0530

    loading page

---
+++ app/Preloader.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+export default function Preloader({ onComplete }: { onComplete: () => void }) {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          setTimeout(onComplete, 500);
+          return 100;
+        }
+        return prev + 1;
+      });
+    }, 20);
+
+    return () => clearInterval(interval);
+  }, [onComplete]);
+
+  return (
+<motion.div
+  className="fixed inset-0 bg-[#1B1B1B] flex flex-col items-center justify-center text-white z-50"
+  initial={{ opacity: 1 }}
+  animate={progress === 100 ? { opacity: 0 } : {}}
+  transition={{ duration: 0.5, ease: "easeOut" }}
+>
+{/* Solid Color Progress Bar with Opacity Animation */}
+<motion.div
+  className="w-60 h-[1px] bg-blue-400 mt-4 relative"
+  animate={{ width: `${progress}%`}}
+  transition={{ ease: "easeInOut" }}
+/>
+
+  {/* Progress percentage in bottom-right */}
+  <motion.div className="absolute bottom-8 right-8 text-6xl font-bold">
+    {progress}%
+  </motion.div>
+</motion.div>
+
+  
+  );
+  
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
 'use client'
+import { useState } from "react";
 import FirstSection from "./sections/first";
 import FourthSection from "./sections/fourth";
 import SecondSection from "./sections/second";
 import ThirdSection from "./sections/third";
+import Preloader from "./Preloader";
 
 
 
 export default function Home() {
+  const [loadingComplete, setLoadingComplete] = useState(false);
+
   return (
-   <div className="geologica">
-    <FirstSection/>
-
-    <SecondSection/>
-
-    <ThirdSection/>
-
-    <FourthSection/>
-   </div>
+    !loadingComplete ? (
+      <Preloader onComplete={() => setLoadingComplete(true)} />
+    ) : (
+      <div className="geologica">
+        <FirstSection/>
+        <SecondSection/>
+        <ThirdSection/>
+        <FourthSection/>
+      </div>
+    )
   );
 }
